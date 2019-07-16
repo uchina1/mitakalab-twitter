d1889642b4a79f5d8b3ff89f9821340fb0d6278d
uchina <uchina1oreryu@gmail.com>
2019-07-17 03:16:15 +0900

    ひととおり

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  def index
-  end
+	def index
+	end
 
-  def show
-  end
+	def show
+		@user = User.find_by(:username => params[:username])
+	end
 end
